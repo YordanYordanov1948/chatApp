c1033da04aeff507e2a2382061db4263b69c98e9
Yordan Yordanov <46786238+YordanYordanov1948@users.noreply.github.com>
2021-03-21 15:23:47 +0200

    clean user function added , grid columns update , side panels updated , userpanel updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,35 @@
 import React from "react";
-import styled from "styled-components";
+import { Grid } from "semantic-ui-react";
+import { connect } from "react-redux";
+import styled from 'styled-components';
 import ColorPanel from "./ColorPanel/ColorPanel";
 import SidePanel from "./SidePanel/SidePanel";
 import Messages from "./Messages/Messages";
 import MetaPanel from "./MetaPanel/MetaPanel";
-import { Grid } from "semantic-ui-react";
 
-const App = () => {
-  return (
-    <Wrapper>
-      <Grid colums="equal" style={{ background: "eee" }}>
-        <ColorPanel />
-        <SidePanel />
+const App = ({ currentUser }) => (
+  <Wrapper> 
+  <Grid columns="equal" style={{ background: "#eee" }}>
+    <ColorPanel />
+    <SidePanel currentUser={currentUser} />
 
-        <Grid.Column style={{ marginLeft: 220 }}>
-          <Messages />
-        </Grid.Column>
+    <Grid.Column style={{ marginLeft: 320 }}>
+      <Messages />
+    </Grid.Column>
 
-        <Grid.Column width={4} style={{ marginLeft: 320 }}>
-          <MetaPanel />
-        </Grid.Column>
-      </Grid>
-    </Wrapper>
-  );
-};
+    <Grid.Column width={4}>
+      <MetaPanel />
+    </Grid.Column>
+  </Grid>
+  </Wrapper> 
+);
 
-export default App;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(App);
+
 
 const Wrapper = styled.div`
   margin: 20px;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
-import Spinner from "./Spinner";
 import reportWebVitals from "./reportWebVitals";
+import Spinner from "./Spinner";
+
 import firebase from "./firebase";
 
 import "semantic-ui-css/semantic.min.css";
@@ -13,23 +14,26 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  withRouter,
+  withRouter
 } from "react-router-dom";
 
 import { createStore } from "redux";
 import { Provider, connect } from "react-redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./reducers";
-import { setUser } from "./actions";
+import { setUser, clearUser } from "./actions";
 
 const store = createStore(rootReducer, composeWithDevTools());
 
 class Root extends React.Component {
   componentDidMount() {
-    firebase.auth().onAuthStateChanged((user) => {
+    firebase.auth().onAuthStateChanged(user => {
       if (user) {
-        this.props.setUser(user);
+         this.props.setUser(user);
         this.props.history.push("/");
+      } else {
+        this.props.history.push("/login");
+        this.props.clearUser();
       }
     });
   }
@@ -47,11 +51,17 @@ class Root extends React.Component {
   }
 }
 
-const mapStateFromProps = (state) => ({
-  isLoading: state.user.isLoading,
+const mapStateFromProps = state => ({
+  isLoading: state.user.isLoading
 });
 
-const RootWithAuth = withRouter(connect(mapStateFromProps, { setUser })(Root));
+const RootWithAuth = withRouter(
+  connect(
+    mapStateFromProps,
+    { setUser, clearUser }
+  )(Root)
+);
+
 
 ReactDOM.render(
   <Provider store={store}>
