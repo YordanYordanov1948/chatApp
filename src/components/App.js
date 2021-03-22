00cf5a3302faa8f4982538675ef55ad307b5d7bd
Yordan Yordanov <46786238+YordanYordanov1948@users.noreply.github.com>
2021-03-22 13:43:33 +0200

    Channels updated , Meta panel added , messasges header updated , message form added

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,41 @@
 import React from "react";
 import { Grid } from "semantic-ui-react";
 import { connect } from "react-redux";
-import styled from 'styled-components';
+import styled from "styled-components";
 import ColorPanel from "./ColorPanel/ColorPanel";
 import SidePanel from "./SidePanel/SidePanel";
 import Messages from "./Messages/Messages";
 import MetaPanel from "./MetaPanel/MetaPanel";
 
-const App = ({ currentUser }) => (
-  <Wrapper> 
-  <Grid columns="equal" style={{ background: "#eee" }}>
-    <ColorPanel />
-    <SidePanel currentUser={currentUser} />
+const App = ({ currentUser , currentChannel }) => (
+  <Wrapper>
+    <Grid columns="equal" style={{ background: "#eee" }}>
+      <ColorPanel />
+      <SidePanel
+        key={currentUser && currentUser.uid}
+        currentUser={currentUser}
+      />
 
-    <Grid.Column style={{ marginLeft: 320 }}>
-      <Messages />
-    </Grid.Column>
+      <Grid.Column style={{ marginLeft: 320 }}>
+      <Messages
+          key={currentChannel && currentChannel.id}
+          currentChannel={currentChannel}
+          currentUser={currentUser}
+        />
+      </Grid.Column>
 
-    <Grid.Column width={4}>
-      <MetaPanel />
-    </Grid.Column>
-  </Grid>
-  </Wrapper> 
+      <Grid.Column width={4}>
+        <MetaPanel />
+      </Grid.Column>
+    </Grid>
+  </Wrapper>
 );
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
 });
 
 export default connect(mapStateToProps)(App);
-
 
 const Wrapper = styled.div`
   margin: 20px;
--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -1,17 +1,54 @@
 import React from "react";
+import firebase from "../../firebase";
 import { Menu, Icon, Modal, Form, Input, Button } from "semantic-ui-react";
 
 class Channels extends React.Component {
   state = {
+    user: this.props.currentUser,
     channels: [],
     channelName: "",
     channelDetails: "",
-    modal: false
+    channelsRef: firebase.database().ref("channels"),
+    modal: false,
   };
 
-  handleChange = event => {
+  addChannel = () => {
+    const { channelsRef, channelName, channelDetails, user } = this.state;
+
+    const key = channelsRef.push().key;
+
+    const newChannel = {
+      id: key,
+      name: channelName,
+      details: channelDetails,
+    };
+
+    channelsRef
+      .child(key)
+      .update(newChannel)
+      .then(() => {
+        this.setState({ channelName: "", channelDetails: "" });
+        this.closeModal();
+        console.log("channel added");
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    if (this.isFormValid(this.state)) {
+      this.addChannel();
+    }
+  };
+
+  handleChange = (event) => {
     this.setState({ [event.target.name]: event.target.value });
   };
+
+  isFormValid = ({ channelName, channelDetails }) =>
+    channelName && channelDetails;
 
   openModal = () => this.setState({ modal: true });
 
@@ -29,14 +66,12 @@ class Channels extends React.Component {
             </span>{" "}
             ({channels.length}) <Icon name="add" onClick={this.openModal} />
           </Menu.Item>
-        
         </Menu.Menu>
 
-      
         <Modal basic open={modal} onClose={this.closeModal}>
           <Modal.Header>Add a Channel</Modal.Header>
           <Modal.Content>
-            <Form>
+            <Form onSubmit={this.handleSubmit}>
               <Form.Field>
                 <Input
                   fluid
@@ -58,10 +93,10 @@ class Channels extends React.Component {
           </Modal.Content>
 
           <Modal.Actions>
-            <Button color="#00FFFF" inverted>
+            <Button color="green" inverted onClick={this.handleSubmit}>
               <Icon name="checkmark" /> Add
             </Button>
-            <Button color="#DEB887" inverted onClick={this.closeModal}>
+            <Button color="red" inverted onClick={this.closeModal}>
               <Icon name="remove" /> Cancel
             </Button>
           </Modal.Actions>
